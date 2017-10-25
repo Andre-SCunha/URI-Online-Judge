b0b199c868f3c8c986b076d2a5c5b66bee122178
Andre-SCunha <brayils.andre@gmail.com>
2017-10-25 16:51:36 -0200

    Update main.cpp

--- 1364_Emoticons :-)/main.cpp
+++ 1364_Emoticons :-)/main.cpp
@@ -9,18 +9,48 @@ typedef TrieNode* pTrieNode;
 
 struct TrieNode{
     char c;
-    int size;
+    int size, fill;
     list<pTrieNode> child;
+    pTrieNode sufix, dicsuf;
 
-    TrieNode(char _c, int _size): c(_c), size(_size) {};
+    TrieNode(char _c, int _size): c(_c), size(_size), fill(0), sufix(NULL), dicsuf(NULL) {};
 };
 
 struct Trie{
     pTrieNode root;
+    list<pTrieNode> elements;
+
+    Trie();
+    void Clear();
+    void addNode(pTrieNode parent, char c);
 };
 
 int main (){
 
 
     return 0;
+}
+
+Trie::Trie(){
+
+    root = new TrieNode('\0', 0);
+    elements.push_back(root);
+
+}
+
+void Trie::Clear(){
+
+    for(pTrieNode node : elements){
+        delete node;
+    }
+    elements.clear();
+
+}
+
+void Trie::addNode(pTrieNode parent, char c){
+
+    pTrieNode node = new TrieNode(c, parent->size+1);
+    elements.push_back(node);
+    parent->child.push_back(node);
+
 }
